62e6c23e1fdcd66948bce0265e09b870930156e9
Kurt Griffiths <mail@kgriffs.com>
2020-05-07 10:57:43 -0600

    test: Fix failing link element test
    Signed-off-by: Kurt Griffiths <mail@kgriffs.com>

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn more link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('Learn More About Indicio.tech');
   expect(linkElement).toBeInTheDocument();
 });
